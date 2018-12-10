890dd6a80577597959608d96b134c11f58a7a498
anejaf <aneja.furlan@gmail.com>
2018-12-10 16:06:55 +0100

    py exercises01 v01

--- exercises/python/01_intro/exercises_01.py
+++ exercises/python/01_intro/exercises_01.py
@@ -3,11 +3,10 @@ def is_palindrome(string_var):
     '''
     returns True if string_var is palindrome, False otherwise
     '''
-    ret = None
-    ########################
-    # code here
-    ########################
-    return ret 
+    for i in range(int(len(string_var) / 2)):
+        if string_var[i] != string_var[-1-i]:
+            return False
+    return True
 
 def histogram_letters(string_var):     
     '''
@@ -15,42 +14,36 @@ def histogram_letters(string_var):
     and returns a dict
 
     '''
-    ret = None
-    ########################
-    # code here
-    ########################
-    return ret 
+    hist = {}
+    for letter in string_var:
+        if letter in hist.keys():
+            hist[letter] += 1
+        else:
+            hist[letter] = 1
+    return hist
 
 def get_most_frequent(list_var):
     '''
     returns a tuple with the most frequent object in the list with the 
     corresponing number
+    as it is now, it returns only one of the max elements if there are multiple
     '''
-    ret = None
-    ########################
-    # code here
-    ########################
-    return ret
+    return max(histogram_letters(list_var).items(), key=lambda k: k[1])
 
 def which_duplicates(list_var):
     '''
     return a dict with containing the repeted object and the number of repetitions
     '''
-    ret = None
-    ########################
-    # code here
-    ########################
-    return ret
+    return dict([el for el in histogram_letters(list_var).items() if el[1] > 1])
 
 
 def compute_factorial(int_val):
     '''
     returns factorial of a number
     '''
-    ret = None
-    ########################
-    # code here
-    ########################
+    ret = 1
+    for i in range(1, int_val+1):
+        ret *= i
     return ret
 
 
@@ -58,11 +51,16 @@ def is_prime(int_val):
     '''
     returns True if int_val is prime, False otherwise
     '''
-    ret = None
-    ########################
-    # code here
-    ########################
-    return ret
+    if int_val < 4:
+        return int_val > 1
+    if int_val % 2 == 0 or int_val % 3 == 0:
+        return False
+    i = 5
+    while i*i <= int_val:
+        if int_val % i == 0 or int_val % (i+2) == 0:
+            return False
+        i += 1
+    return True
 
         
 
